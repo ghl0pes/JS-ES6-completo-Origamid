29e01b81c0fff2f87dec31b49db1d2bd77c531fb
Gabriel Lopes <gabriellopes1005@gmail.com>
2022-01-07 12:09:18 -0300

    Add accordion list in FAQ

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-const tabmenu = document.querySelectorAll('.js-tabmenu li');
-const tabcontent = document.querySelectorAll('.js-tabcontent section');
+function initTabNav(){
+    const tabmenu = document.querySelectorAll('.js-tabmenu li');
+    const tabcontent = document.querySelectorAll('.js-tabcontent section');
 
-if(tabmenu.length && tabcontent.length){
-    tabcontent[0].classList.add('ativo');
+    if(tabmenu.length && tabcontent.length){
+        tabcontent[0].classList.add('ativo');
 
-    function activeTab(index){
-        tabcontent.forEach((section) => {
-            section.classList.remove('ativo');
+        function activeTab(index){
+            tabcontent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+            tabcontent[index].classList.add('ativo');
+        }
+
+        tabmenu.forEach((itemMenu, index) =>{
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
         });
-        tabcontent[index].classList.add('ativo');
     }
-
-    tabmenu.forEach((itemMenu, index) =>{
-        itemMenu.addEventListener('click', () => {
-            activeTab(index);
-        });
-    });
 }
+
+initTabNav();
+
+function initAccordion(){
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClass = 'ativo';
+    if(accordionList.length){
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass);
+
+        function activeAccordion(){
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        });
+    }
+}
+
+initAccordion();
